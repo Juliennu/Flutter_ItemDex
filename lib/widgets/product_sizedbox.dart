777cd593a45ba79f010185ab201461633d0e335f
Juliennu <wsb.1099@gmail.com>
2023-08-18 18:52:52 +0900

    セルタップ時にポップアップを表示

--- lib/widgets/product_sizedbox.dart
+++ lib/widgets/product_sizedbox.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:item_dex/models/product.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-class ProductSizedBox extends StatelessWidget {
+List<HitProduct> myItems = [];
+
+class ProductSizedBox extends StatefulWidget {
   const ProductSizedBox({
     super.key,
     required this.hitProduct,
@@ -10,47 +13,87 @@ class ProductSizedBox extends StatelessWidget {
   final HitProduct hitProduct;
 
   @override
+  State<ProductSizedBox> createState() => _ProductSizedBoxState();
+}
+
+class _ProductSizedBoxState extends State<ProductSizedBox> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          SizedBox(
-            height: 80,
-            width: 80,
-            child: Image.network(hitProduct.image.medium),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 8.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    hitProduct.name,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
+    Row row = Row(
+      children: [
+        SizedBox(
+          height: 80,
+          width: 80,
+          child: Image.network(widget.hitProduct.image.medium),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.hitProduct.name,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(
-                    height: 4,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  widget.hitProduct.brand.name,
+                  style: const TextStyle(
+                    fontSize: 16,
                   ),
-                  Text(
-                    hitProduct.brand.name,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
+      ],
+    );
+
+    Alert alert = Alert(
+      context: context,
+      image: SizedBox(
+        height: 150,
+        width: 150,
+        child: Image.network(widget.hitProduct.image.medium),
+      ),
+      title: widget.hitProduct.name,
+      buttons: [
+        DialogButton(
+          child: const Text(
+            '所持品に追加',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          onPressed: () {
+            setState(() {
+              myItems.add(widget.hitProduct);
+              Navigator.pop(context);
+            });
+          },
+        ),
+      ],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {
+          alert.show();
+        },
+        child: row,
       ),
     );
   }
