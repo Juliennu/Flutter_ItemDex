300f9e95f4551f8119035804110bb40291078bd4
Juliennu <wsb.1099@gmail.com>
2023-08-25 15:49:11 +0900

    update searchScreen into StatelessWidget

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -4,60 +4,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http; // httpという変数を通して、httpパッケージにアクセス
 import 'package:item_dex/models/product.dart'; // 作成したモデル
+import 'package:item_dex/models/product_data.dart';
 import 'package:item_dex/widgets/item_dex_text.dart';
 import 'package:item_dex/widgets/product_sizedbox.dart';
+import 'package:provider/provider.dart';
 
-class SearchScreen extends StatefulWidget {
+class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
-  State<SearchScreen> createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> {
-  final itemDexText = ItemDexText();
-  List<HitProduct> hitProducs = [];
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: itemDexText.createHeaderTitle('商品検索'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 12,
-              horizontal: 20,
-            ),
-            child: TextField(
-              autocorrect: false,
-              autofocus: true,
-              style: const TextStyle(
-                fontSize: 18,
-              ),
-              decoration: const InputDecoration(
-                hintText: '商品名、メーカー名を入力',
-                icon: Icon(Icons.search),
-              ),
-              onSubmitted: (String value) async {
-                final result = await searchProduct(value);
-                setState(() {
-                  hitProducs = result;
-                });
-              },
-            ),
+    final itemDexText = ItemDexText();
+
+    return Consumer<ProductData>(
+      builder: (context, productData, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: itemDexText.createHeaderTitle('商品検索'),
           ),
-          Expanded(
-            child: ListView(
-              children: hitProducs
-                  .map((hitProduct) => ProductSizedBox(hitProduct: hitProduct))
-                  .toList(),
-            ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 12,
+                  horizontal: 20,
+                ),
+                child: TextField(
+                  autocorrect: false,
+                  autofocus: true,
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                  decoration: const InputDecoration(
+                    hintText: '商品名、メーカー名を入力',
+                    icon: Icon(Icons.search),
+                  ),
+                  onSubmitted: (String value) async {
+                    final result = await searchProduct(value);
+                    productData.updateProductsSearchResult(result);
+                  },
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  children: productData.productsSearchResult
+                      .map(
+                        (hitProduct) => ProductSizedBox(
+                          hitProduct: hitProduct,
+                          dialogButtonOnTapCallback: () {
+                            productData.addMyItem(hitProduct);
+                          },
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 
