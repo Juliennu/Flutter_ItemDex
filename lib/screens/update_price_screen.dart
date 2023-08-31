8bc9a47c39032b50477b554ca79d5e2395ec3327
Juliennu <wsb.1099@gmail.com>
2023-08-31 10:22:54 +0900

    add update_price_screen

---
+++ lib/screens/update_price_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UpdatePriceScreen extends StatelessWidget {
+  final int? purchasePrice;
+  final Function(int?) onOKPressed;
+
+  const UpdatePriceScreen({
+    super.key,
+    this.purchasePrice,
+    required this.onOKPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    int? updatedPrice = purchasePrice;
+
+    // keyboard をスクロール範囲に入れる
+    return SingleChildScrollView(
+      child: Container(
+        // keyboard の上に要素を表示するようにする(MediaQuery)
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: IntrinsicWidth(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  // controller: TextEditingController(
+                  //   text: updatedPrice.toString(),
+                  // ),
+                  // 入力ソースを数値のみに制限
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  autofocus: true,
+                  autocorrect: false,
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 4,
+                      ),
+                    ),
+                  ),
+                  onChanged: (priceText) {
+                    int? price = int.parse(priceText);
+                    updatedPrice = price;
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FilledButton(
+                  style: FilledButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(0),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    onOKPressed(updatedPrice);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
